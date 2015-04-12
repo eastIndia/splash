83a8aab8552ef3872f47b983d673b9beca69aac2
Michael Ruddy <m.ruddy@brawl.me>
2015-04-11 23:32:04 -0600

    hello world

---
+++ app/js/app.js
@@ -0,0 +1,16 @@
+angular.module('eastIndia', [
+    'ngAnimate',
+    'ui.router',
+    'snap',
+    'angular-carousel'
+]);
+
+
+
+angular.module('eastIndia').config(function($stateProvider, $urlRouterProvider) {
+
+    "use strict";
+
+});
+
+
---
+++ app/js/controllers/mainController.js
@@ -0,0 +1,5 @@
+angular.module('eastIndia').controller('mainController', function ($scope, $rootScope) {
+    "use strict";
+
+    $scope.message = "Yeahhh ! You're ready !";
+});
